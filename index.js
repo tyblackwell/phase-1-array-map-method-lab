957fb644f7763b204afaf713338e4627f257068f
Ty Blackwell <tyeverettblackwell@gmail.com>
2023-05-02 17:56:31 -0400

    commit tree 01b080aea5bf6b1806738b780f52d53d9f008cd6

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
+function titleCased() {
+  const tutorials = [
+    "what does the this keyword mean?",
+    "What is the Constructor OO pattern?",
+    "implementing Blockchain Web API",
+    "The Test Driven Development Workflow",
+    "What is NaN and how Can we Check for it",
+    "What is the difference between stopPropagation and preventDefault?",
+    "Immutable State and Pure Functions",
+    "what is the difference between == and ===?",
+    "what is the difference between event capturing and bubbling?",
+    "what is JSONP?",
+  ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCasedTutorials = tutorials.map((tutorial) => {
+  const words = tutorial.split(" ");
+  const capitalizedWords = words.map((word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  });
+  return capitalizedWords.join(" ");
+});
+
+return titleCasedTutorials;
+
+console.log(titleCased());
+};
